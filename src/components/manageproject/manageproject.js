f44719bf2f19f45116151ae321c28be029a9948f
bavnsoft <bavnsofts@gmail.com>
2019-05-29 23:32:17 +0530

    .
    .

--- src/components/manageproject/manageproject.js
+++ src/components/manageproject/manageproject.js
@@ -14,6 +14,7 @@ class manageproject extends Component {
    constructor(props){
      super(props);
      this.state = {
+      shareholders: [{ name: "" }],
 
       Progress:'',
       Complete:'',
@@ -101,11 +102,105 @@ addst(){
  
 
 
-  updateStatus(id){
+  get_project_id(id){
+
+       this.setState({project_id:id})
+
+  }
+
+
+  addmilestone(milestone){
+
+     axios.post(config.LiveapiUrl+'addmilestone', {milestone:this.state.shareholders,id:this.state.project_id})
+          .then((result) => {
+            //access the results here....
+
+            console.log(result.data.user_id);
+            if(result.data.status==true){
+                swal(result.data.message);
+               
+                
+            }else{
+                    swal(result.data.message);
+
+            }
+
+          });
+  }
+ 
+
+  updatemilestone(id){
 
     this.setState({project_id:id})
 
   }
+ getMilestone(id,milestone){
+    if(milestone){    
+       this.setState({project_id:id,shareholders:JSON.parse(milestone)});
+    }else{
+     this.setState({project_id:id,shareholders: [{ name: "" }]});
+    }
+  }
+
+
+
+
+
+  handleNameChange = evt => {
+    this.setState({ name: evt.target.value });
+  };
+
+  handleShareholderNameChange = idx => evt => {
+    const newShareholders = this.state.shareholders.map((shareholder, sidx) => {
+      if (idx !== sidx) return shareholder;
+      return { ...shareholder, name: evt.target.value };
+    });
+
+    this.setState({ shareholders: newShareholders });
+  };
+
+
+
+
+handleShareholderDetail = idx => evt => {
+    const newShareholders = this.state.shareholders.map((shareholder, sidx) => {
+      if (idx !== sidx) return shareholder;
+      return { ...shareholder, detail: evt.target.value };
+    });
+
+    this.setState({ shareholders: newShareholders });
+  };
+
+
+
+
+  handleShareholderHours = idx => evt => {
+    const newShareholders = this.state.shareholders.map((shareholder, sidx) => {
+      if (idx !== sidx) return shareholder;
+      return { ...shareholder, hours: evt.target.value };
+    });
+
+    this.setState({ shareholders: newShareholders });
+  };
+
+
+
+  handleSubmit = evt => {
+    const { name, shareholders } = this.state;
+    alert(`Incorporated: ${name} with ${shareholders.length} shareholders`);
+  };
+
+  handleAddShareholder = () => {
+    this.setState({
+      shareholders: this.state.shareholders.concat([{ name: "" }])
+    });
+  };
+
+  handleRemoveShareholder = idx => () => {
+    this.setState({
+      shareholders: this.state.shareholders.filter((s, sidx) => idx !== sidx)
+    });
+  };
 
 
   render() {
@@ -127,28 +222,28 @@ addst(){
               <div className="modal-header">
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span></button>
-                <h4 className="modal-title">Department Changes</h4>
+                <h3 className="modal-title">Add Stauts</h3>
               </div>
               <div className="modal-body">
-                <button type="button" value="Progress" className="btn btn-primary"    onClick={(e)=>this.addstatus('Progress')} >Progress</button>&nbsp;
+                
+             
+<div>
+        <label>Select Stauts</label>
+        <select className="form-control select2" style={{width: '100%'}}>
+          <option selected="selected">select</option>
+          <option>Progress</option>
+          >
+          <option>Complete</option>
+          <option>Canceled</option>
+        </select>
+      </div>
 
 
-                <button type="button" className="btn btn-primary"  onClick={(e)=>this.addstatus('Complete')} >Complete
-
- <StarRatingComponent 
-                    name="rate1" 
-                    starCount={5}
-                    value={rating}
-                    onStarClick={this.onStarClick.bind(this)} />
-
-                </button>&nbsp;
-
-                <button type="button" className="btn btn-primary"   onClick={(e)=>this.addstatus('Cancel')} >Cancel</button>&nbsp;
               </div>
 
               <div className="modal-footer">
                 {/*<button type="button" className="btn btn-default pull-left" data-dismiss="modal">Close</button>*/}
-                <button type="button" className="btn btn-primary"data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary"data-dismiss="modal" onClick={(e)=>this.addstatus('Progress')}>Apply</button>
               </div>
             </div>         
              
@@ -165,14 +260,110 @@ addst(){
               <div className="modal-header">
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span></button>
-                <h4 className="modal-title">Department Changes</h4>
+                <h4 className="modal-title">Add Milestone</h4>
               </div>
               <div className="modal-body">
-                <button type="button" className="btn btn-primary"data-dismiss="modal">Add Milestone</button>&nbsp;
+                       
+
+
+                       <div className="row">
+
+               <div className="col-md-12">
+
+            {/* general form elements */}
+            <div className="box box-primary">
+              <div className="box-header with-border">
+                <div className="row">
+                <div className="col-md-8">
+
+               
+               </div>
+              <div className="col-md-4">
+        
+         <button
+          type="button" className="btn btn-primary"
+          onClick={this.handleAddShareholder}
+          >
+          ADD
+        </button>
+             </div>
+
+              </div></div>
+              {/* /.box-header */}
+              {/* form start */}
+               <form onSubmit={this.handleSubmit}>
+      {/*  <input
+          type="text"
+          placeholder="Bavn Soft"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />*/}
+
+        <h4>Milestone Task</h4>
+
+        {this.state.shareholders.map((shareholder, idx) => (
+          <div className="shareholder">
+            <div className="row">
+                <div className="col-md-4">
+            <input
+              type="text"
+              placeholder={`Milestone ${idx + 1} Name`}
+              value={shareholder.name}
+              
+
+
+              onChange={this.handleShareholderNameChange(idx)}
+            />
+            </div>
+
+            
+            <div className="col-md-3">
+                <input
+                  type="text"
+                  placeholder={`Milestone Detail`}
+                  value={shareholder.detail}
+                  onChange={this.handleShareholderDetail(idx)}
+                />
+            </div>
+
+            <div className="col-md-3">
+                <input
+                  type="text"
+                  placeholder={`Hours`}
+                  value={shareholder.hours}
+                  onChange={this.handleShareholderHours(idx)}
+                />
+            </div>
+
+            <div className="col-md-2">
+                <button
+                  type="button" className="btn btn-danger"
+                  onClick={this.handleRemoveShareholder(idx)}
+                  className="small">
+                
+              <i className="fa fa-times"></i>
+
+               </button>
+              </div>
+
+
+          </div>              
+       </div>
+        ))}
+     <center>
+     <button type="button" className="btn btn-primary submit" onClick={(e)=>this.addmilestone(e)}>Submit</button>
+</center>
+      </form>
+           </div>
+
+            </div>
+          </div>
+
+
+
               </div>
               <div className="modal-footer">
-                <button type="button" className="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary"data-dismiss="modal">Update</button>
+                <button type="button" className="btn btn-primary"data-dismiss="modal">Close</button>
               </div>
             </div>         
              
@@ -223,8 +414,8 @@ addst(){
                     <td>{item.startdate}</td>
                       <td>{item.enddate}</td>
                         <td><img src={item.upload } style={{height:"50px",width:"50px"}}/></td>
-                <td> <button type="button" className="btn btn-primary"data-toggle="modal" data-target="#modal-defaults">View <i className="fa fa-eye"></i></button></td>
-                <td> <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#modal-default" onClick={(e)=>this.updateStatus(item._id)}>Status </button></td>
+                <td> <button type="button" className="btn btn-primary"data-toggle="modal" data-target="#modal-defaults" onClick={(e)=>this.getMilestone(item._id,item.milestone)}>Add Milestone</button></td>
+                <td> <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#modal-default" onClick={(e)=>this.get_project_id(item._id)}>Status </button></td>
               </tr>
               
                 ))}
